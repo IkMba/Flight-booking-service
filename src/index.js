ed497c6e79037d37cb3fd246dd0c09ab1ff85b06
IkMba <mariahchester89@gmail.com>
2025-02-23 10:01:39 +0100

    created project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-const express = require('express');
+const express = require("express");
 
-const { ServerConfig } = require('./config');
-const apiRoutes = require('./routes');
+const { ServerConfig } = require("./config");
+const apiRoutes = require("./routes");
+const serverConfig = require("./config/server-config");
 
 const app = express();
 
-app.use('/api', apiRoutes);
+app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
-    console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+  console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
 });
