8b2aa2f1efe3c21c63d90150d15724280305d6d1
IkMba <mariahchester89@gmail.com>
2025-02-25 09:50:02 +0100

    added booking pay

--- src/routes/v1/booking.js
+++ src/routes/v1/booking.js
@@ -7,13 +7,12 @@ const router = express.Router();
 
 router.post(
     '/',
-    // BookingService.createBooking
     BookingController.createBooking
 )
 
-// router.post(
-//     '/payments',
-//     BookingController.makePayment
-// );
+router.post(
+    '/payments',
+    BookingController.makePayment
+);
 
 module.exports = router;
--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -24,7 +24,7 @@ async function createBooking(data) {
         const booking = await bookingRepository.create(bookingPayload, transaction);
 
         // console.log(flightData)
-        console.log(booking)
+        // console.log(booking)
         await axios.patch(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}/seats`, {
             seats: data.noOfSeats
         });
@@ -38,6 +38,41 @@ async function createBooking(data) {
     
 }
 
+async function makePayment(data) {
+    const transaction = await db.sequelize.transaction();
+    try {
+        const bookingDetails = await bookingRepository.get(data.bookingId, transaction);
+        if(bookingDetails.status == CANCELLED) {
+            throw new AppError('The booking has expired', StatusCodes.BAD_REQUEST);
+        }
+        console.log(bookingDetails);
+        const bookingTime = new Date(bookingDetails.createdAt);
+        const currentTime = new Date();
+        // if(currentTime - bookingTime > 300000) {
+        //     await cancelBooking(data.bookingId);
+        //     throw new AppError('The booking has expired', StatusCodes.BAD_REQUEST);
+        // }
+        if(bookingDetails.totalCost != data.totalCost) {
+            throw new AppError('The amount of the payment doesnt match', StatusCodes.BAD_REQUEST);
+        }
+        if(bookingDetails.userId != data.userId) {
+            throw new AppError('The user corresponding to the booking doesnt match', StatusCodes.BAD_REQUEST);
+        }
+        // we assume here that payment is successful
+        await bookingRepository.update(data.bookingId, {status: BOOKED}, transaction);
+        // Queue.sendData({
+        //     recepientEmail: 'cs191297@gmail.com',
+        //     subject: 'Flight booked',
+        //     text: `Booking successfully done for the booking ${data.bookingId}`
+        // });
+        await transaction.commit();
+        
+    } catch(error) {
+        await transaction.rollback();
+        throw error;
+    }
+}
+
 module.exports = {
-    createBooking
+    createBooking,makePayment
 }
